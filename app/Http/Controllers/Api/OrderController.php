97964aa6377af226cb097291f58311f29cc4597e
Ng Kai Zhe <kaizhe1991@hotmail.com>
2018-11-30 00:55:40 +0800

    small changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -24,7 +24,51 @@ class OrderController extends Controller
     public function create(Request $request)
     {
 
+        //get variable from request parameter
 
+        $user_id = $request->input('user_id');
+
+        $address = $request->input('address');
+
+        $phone_number = $request->input('phone_number');
+
+        $total_price = $request->input('total_price');
+
+        //search the user in shopping cart to get what he has in his shopping cart
+        $product_ids = array();
+        $shoppingcarts = Shoppingcart::where('user_id', $user_id)->get();
+
+        foreach ($shoppingcarts as $shoppingcart) {
+            $temp  = $shoppingcart->product_id;
+            array_push($product_ids, $temp);
+        }
+
+        //get quantities
+        $quantites = array();
+
+        foreach ($shoppingcarts as $shoppingcart) {
+            $temp  = $shoppingcart->quantity;
+            array_push($quantites, $temp);
+        }
+
+        //now we have quantities and product_ids
+
+        //create new order
+        //copy all value to order
+        $order = new Order([
+            'user_id' =>$user_id,
+            'address' => $address,
+            'phone_number' => $phone_number,
+            'is_check' => false,
+            'total_price' =>$total_price,
+            'product_ids' => $product_ids,
+            'quantities' => $quantites,
+        ]);
+
+        $order->save();
+
+        //delete shoppingcart move must be done
+        //still remain undone as this is a debug mode
         
     }
 
